4bab3e5cd082d918d54dd6f7b3250fc8431b4ab1
Timur <timur.shevtsov1984@gmail.com>
2021-03-13 13:14:08 +0100

    add breadcrumbs component

---
+++ resources/views/components/breadcrumbs.blade.php
@@ -0,0 +1,15 @@
+@unless ($breadcrumbs->isEmpty())
+
+    <ol class="breadcrumb bg-transparent">
+        @foreach ($breadcrumbs as $breadcrumb)
+
+            @if (!is_null($breadcrumb->url) && !$loop->last)
+                <li class="breadcrumb-item"><a class="text-dark" href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+            @else
+                <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
+            @endif
+
+        @endforeach
+    </ol>
+
+@endunless
